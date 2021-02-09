05f1af9fd9fddb625871b11b146a2ddeb89acc2b
jejaksaid <said@fastlearningcenter.com>
2021-02-10 00:20:50 +0700

    main js

--- restaurant-website/assets/js/main.js
+++ restaurant-website/assets/js/main.js
@@ -42,3 +42,19 @@ function scrollActive() {
     })
 }
 window.addEventListener('scroll', scrollActive)
+
+// change background header
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    // when the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+// show scroll top
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    // when the scrol is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
